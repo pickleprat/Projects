611ceb3c7fe461affd577cf19aa950b94957dfd5
pickleprat <prat16here@gmail.com>
2023-01-23 18:58:39 +0530

    making it public

--- hotel_reservation/client/main.py
+++ hotel_reservation/client/main.py
@@ -40,5 +40,5 @@ def home():
     return render_template('index.html', prediction = convert(prediction[0]))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0')
     
